f4b098eeb6350caa27af79c0877834d504f4b8eb
Killian <oss@xtrm.me>
2023-12-18 17:54:59 +0100

    📦 deps(libft): pull remote

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: kiroussa <oss@xtrm.me>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 23:34:19 by kiroussa          #+#    #+#             */
-/*   Updated: 2023/12/02 23:58:25 by kiroussa         ###   ########.fr       */
+/*   Updated: 2023/12/18 17:52:21 by kiroussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int	main(int argc, char *argv[], char *envp[])
 	(void) envp;
 	msh_signal_init();
 	msh_shell_prompt();
+	clear_history();
+	rl_clear_history();
 	ft_putendl("exit");
 	return (0);
 }

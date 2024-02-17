327d7995872f9f24379ef2b0f786e3594710aca0
xtrm <oss@xtrm.me>
2024-02-17 05:49:52 +0100

    🐛 fix(cli): fixed non-interactive mode where input would still echo out
    Signed-off-by: xtrm <oss@xtrm.me>

--- submodules/cli/src/shell/msh_shell_loop.c
+++ submodules/cli/src/shell/msh_shell_loop.c
@@ -6,11 +6,13 @@
 /*   By: kiroussa <oss@xtrm.me>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 05:16:25 by kiroussa          #+#    #+#             */
-/*   Updated: 2024/02/17 05:19:56 by kiroussa         ###   ########.fr       */
+/*   Updated: 2024/02/17 05:48:18 by kiroussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <msh/minishell.h>
+#define MSH_CLI_UTILS
+#include <msh/cli/cli.h>
 
 void	msh_shell_loop(t_minishell *msh)
 {
@@ -19,6 +21,10 @@ void	msh_shell_loop(t_minishell *msh)
 
 	(void) msh;
 	prompt = NULL;
+	if (msh_is_interactive())
+		prompt = "cool prompt> ";
+	else
+		rl_prep_term_function = 0;
 	while (true)
 	{
 		line = readline(prompt);

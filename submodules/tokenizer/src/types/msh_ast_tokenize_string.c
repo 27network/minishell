08b099cac2c7fd185ab7e4961dfbecf0558d2c63
Killian <oss@xtrm.me>
2024-02-26 16:47:41 +0100

    🔨 chore: wow

--- submodules/tokenizer/src/types/msh_ast_tokenize_string.c
+++ submodules/tokenizer/src/types/msh_ast_tokenize_string.c
@@ -6,13 +6,29 @@
 /*   By: kiroussa <oss@xtrm.me>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 16:44:46 by kiroussa          #+#    #+#             */
-/*   Updated: 2024/02/25 19:43:27 by kiroussa         ###   ########.fr       */
+/*   Updated: 2024/02/26 14:08:51 by kiroussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft/string.h>
 #define TOKENIZER_TYPES
 #include <msh/ast/tokenizer.h>
+
+static size_t	msh_find_string_end(const char *line, size_t start)
+{
+	size_t	end;
+	char	start_char;
+	size_t	inners;
+
+	end = start;
+	inners = 0;
+	start_char = line[start];
+	while (line[end])
+	{
+		if (line[end] == start_char && inn
+	}
+	return (end);
+}
 
 t_token	*msh_ast_tokenize_string(const char *line, size_t *cursor)
 {
@@ -21,21 +37,5 @@ t_token	*msh_ast_tokenize_string(const char *line, size_t *cursor)
 	size_t	end;
 
 	start = *cursor;
-	end = start;
-	while (line[end] && line[end] != '\'')
-	{
-		if (line[end] == '\\' && line[end + 1])
-			end += 2;
-		else
-			end++;
-	}
-	if (line[end] == '\'')
-	{
-		end++;
-		token = msh_ast_new_token(TKN_STRING, ft_strndup(line + start, end - start));
-		*cursor = end;
-	}
-	else
-		*cursor = end;
-	return (token);
+	end = msh_find_string_end(line, start);
 }

69a0a3211360ef17639c54e3e9fefda946e853cf
xtrm <oss@xtrm.me>
2024-03-07 07:32:05 +0100

    🐛 fix(cli): fix file runner

--- submodules/cli/src/runner/msh_run_file.c
+++ submodules/cli/src/runner/msh_run_file.c
@@ -6,7 +6,7 @@
 /*   By: kiroussa <oss@xtrm.me>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 01:41:40 by kiroussa          #+#    #+#             */
-/*   Updated: 2024/02/17 05:55:16 by kiroussa         ###   ########.fr       */
+/*   Updated: 2024/03/07 07:31:53 by kiroussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
  * https://github.com/bminor/bash/blob/master/builtins/evalfile.c#L136-L190
  * (checkout 7d7979ba4c2b7848b81f526eac44340909ddd5a1)
  */
-int	msh_run_file(t_minishell *msh, const char *filename)
+void	msh_run_file(t_minishell *msh, const char *filename)
 {
 	char	*resolved;
 
@@ -27,11 +27,11 @@ int	msh_run_file(t_minishell *msh, const char *filename)
 	{
 		ft_dprintf(2, "%s: %s: %s\n", msh->name, filename, "No such file or "
 			"directory");
-		return (127);
+		exit(127);
 	}
 	msh->name = resolved;
 	ft_dprintf(2, "%s: %s: %s\n", msh->name, resolved, "running files or "
 		"scripts is not implemented yet");
 	free(resolved);
-	return (2);
+	exit(2);
 }

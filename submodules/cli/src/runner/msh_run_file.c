e84b1e5f99c78c535e13f1112602b0d2f8060774
xtrm <oss@xtrm.me>
2024-03-10 03:52:10 +0100

    ✨ feature(cli/runner): implement basic validity checks

--- submodules/cli/src/runner/msh_run_file.c
+++ submodules/cli/src/runner/msh_run_file.c
@@ -6,12 +6,13 @@
 /*   By: kiroussa <oss@xtrm.me>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 01:41:40 by kiroussa          #+#    #+#             */
-/*   Updated: 2024/03/10 02:24:53 by kiroussa         ###   ########.fr       */
+/*   Updated: 2024/03/10 03:22:08 by kiroussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <msh/minishell.h>
 #include <msh/io/path.h>
+#include <msh/cli/runner.h>
 
 static int	msh_resolve_fd(t_minishell *msh, const char *filename, char **name)
 {
@@ -37,16 +38,13 @@ static int	msh_resolve_fd(t_minishell *msh, const char *filename, char **name)
 	return (fd);
 }
 
-/**
- * TODO: add those checks: 
- * https://github.com/bminor/bash/blob/master/builtins/evalfile.c#L136-L190
- * (checkout 7d7979ba4c2b7848b81f526eac44340909ddd5a1)
- */
 void	msh_run_file(t_minishell *msh, const char *filename)
 {
-	char		*resolved;
-	int			fd;
+	char	*resolved;
+	int		fd;
+	int		ret;
 
+	ret = 0;
 	fd = msh_resolve_fd(msh, filename, &resolved);
 	if (fd < 0)
 	{
@@ -56,7 +54,12 @@ void	msh_run_file(t_minishell *msh, const char *filename)
 		msh_destroy(msh);
 		exit(127);
 	}
+	msh->name = resolved;
+	if (msh_runner_check(msh, resolved, fd, &ret))
+		ret = msh_run_script(msh, fd, filename);
+	if (resolved)
+		free(resolved);
 	close(fd);
 	msh_destroy(msh);
-	exit(0);
+	exit(ret);
 }

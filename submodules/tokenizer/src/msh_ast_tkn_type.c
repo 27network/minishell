d698ca4de494e1d60cd8cdf7f95b020722f7f7be
Melc-In <80842427+Melc-In@users.noreply.github.com>
2024-03-15 23:41:24 +0100

    ✨ feature(tokenizer): merging words now

--- submodules/tokenizer/src/msh_ast_tkn_type.c
+++ submodules/tokenizer/src/msh_ast_tkn_type.c
@@ -6,7 +6,7 @@
 /*   By: kiroussa <oss@xtrm.me>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 12:16:10 by kiroussa          #+#    #+#             */
-/*   Updated: 2024/03/09 18:28:21 by kiroussa         ###   ########.fr       */
+/*   Updated: 2024/03/15 23:13:02 by cglandus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,14 @@ const char	*msh_ast_tkn_type(t_token_type type)
 	[TKN_NEWLINE] = "TKN_NEWLINE", [TKN_ASSIGN] = "TKN_ASSIGN",
 	[TKN_ASSIGNMENT_WORD] = "TKN_ASSIGNMENT_WORD",
 	[TKN_FD_REF] = "TKN_FD_REF", [TKN_REDIR_OUT] = "TKN_REDIR_OUT",
-	[TKN_REDIR_OUT_CLOBBER] = "TKN_REDIR_OUT_CLOBBER",
-	[TKN_REDIR_OUT_BOTH] = "TKN_REDIR_OUT_BOTH", [TKN_EOF] = "TKN_EOF",
+	[TKN_REDIR_OUT_CLOBBER] = "TKN_REDIR_OUT_CLOBBER", [TKN_EOF] = "TKN_EOF",
 	[TKN_REDIR_APPEND] = "TKN_REDIR_APPEND", [TKN_REDIR_IN] = "TKN_REDIR_IN",
 	[TKN_REDIR_HERE_DOC] = "TKN_REDIR_HERE_DOC",
 	[TKN_REDIR_HERE_STR] = "TKN_REDIR_HERE_STR",
-	[TKN_REDIR_WORD] = "TKN_REDIR_WORD", [TKN_REDIR_FD] = "TKN_REDIR_FD",
-	[TKN_LPAREN] = "TKN_LPAREN", [TKN_RPAREN] = "TKN_RPAREN",
-	[TKN_LBRACKET] = "TKN_LBRACKET", [TKN_RBRACKET] = "TKN_RBRACKET",
-	[TKN_LBRACE] = "TKN_LBRACE", [TKN_RBRACE] = "TKN_RBRACE",
-	[TKN_UNKNOWN] = "TKN_UNKNOWN",
+	[TKN_REDIR_WORD] = "TKN_REDIR_WORD", [TKN_LPAREN] = "TKN_LPAREN",
+	[TKN_RPAREN] = "TKN_RPAREN", [TKN_LBRACKET] = "TKN_LBRACKET",
+	[TKN_RBRACKET] = "TKN_RBRACKET", [TKN_LBRACE] = "TKN_LBRACE", 
+	[TKN_RBRACE] = "TKN_RBRACE",[TKN_UNKNOWN] = "TKN_UNKNOWN"
 	};
 
 	return (types[type]);
